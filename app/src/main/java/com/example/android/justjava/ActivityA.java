22be644a2eed0205d1bdbc3d080666fb7540c7c8
TshepzMogapi <tshepzmogapi@gmail.com>
2019-08-28 23:50:32 +0200

    link onClick to start mainActivity

--- app/src/main/java/com/example/android/justjava/ActivityA.java
+++ app/src/main/java/com/example/android/justjava/ActivityA.java
@@ -1,8 +1,10 @@
 package com.example.android.justjava;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 public class ActivityA  extends Activity {
 
@@ -10,5 +12,13 @@ public class ActivityA  extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+    }
+
+    public void prepareOrder(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
     }
 }
